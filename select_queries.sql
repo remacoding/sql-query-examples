317f1edafe9dc94e111d102075c9448b76edc49a
remacoding <105727870+remacoding@users.noreply.github.com>
2024-06-10 01:43:58 +0300

    select queries

--- select_queries.sql
+++ select_queries.sql
@@ -1,2 +1,7 @@
 -- This file contains various examples of SELECT statements to demonstrate SQL querying capabilities.
 
+select * from heartdisease;
+
+select age, sex from heartdisease
+where age > 1 order by sex asc
+;
