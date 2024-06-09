1aa58cee63b9ba41d3eaefaff82ad8b20fe81322
remacoding <105727870+remacoding@users.noreply.github.com>
2024-06-10 01:44:41 +0300

    deleted file

--- select_queries.sql
+++
@@ -1,7 +0,0 @@
--- This file contains various examples of SELECT statements to demonstrate SQL querying capabilities.
-
-select * from heartdisease;
-
-select age, sex from heartdisease
-where age > 1 order by sex asc
-;
