9d115096c72001000e8cd12e54b88d609b08560a
remacoding <105727870+remacoding@users.noreply.github.com>
2024-06-11 20:49:13 +0300

    select statement

--- select-queries.sql
+++ select-queries.sql
@@ -1,1 +1,9 @@
 -- This file contains various examples of SELECT statements to demonstrate SQL querying capabilities.
+select
+    *
+from
+    heartdisease
+where
+    age > 58
+ORDER BY
+    age asc;
