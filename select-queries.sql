c928c1ca0aa0594f9b82e0ef34cf38219d24bb72
remacoding <105727870+remacoding@users.noreply.github.com>
2024-06-11 22:18:38 +0300

    Lesson 1 (select statements started)

--- select-queries.sql
+++ select-queries.sql
@@ -1,10 +1,10 @@
--- This file contains various examples of SELECT statements to demonstrate SQL querying capabilities.
+-- This file contains 10 examples of SELECT statements to demonstrate SQL querying capabilities.
+
+-- Retrieve all columns from the dataset for the first 10 rows.
 select
     *
 from
     heartdisease
-where
-    age > 58
-ORDER BY
-    age asc;
+limit
+    10;
 
