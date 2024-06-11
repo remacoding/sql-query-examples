9506395f20dec75f251cdcbe0dbed3f43344ce74
remacoding <105727870+remacoding@users.noreply.github.com>
2024-06-11 22:24:23 +0300

    Lesson 1 (select statements) started

--- select-queries.sql
+++ select-queries.sql
@@ -1,5 +1,4 @@
 -- This file contains 10 examples of SELECT statements to demonstrate SQL querying capabilities.
-
 -- Retrieve all columns from the dataset for the first 10 rows.
 select
     *
@@ -8,3 +7,10 @@ from
 limit
     10;
 
+-- Retrieve all records where the patient has heart disease.
+select
+    *
+from
+    heartdisease
+where
+    target = 1;
